e09aa59fd77e6175f0ccc3491320536fd91a5801
ZhangShenao <15201440436@163.com>
2019-12-03 18:09:42 +0800

    Item Comment API

---
+++ eshop-api/src/main/java/william/eshop/controller/ItemCommentController.java
@@ -0,0 +1,43 @@
+package william.eshop.controller;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import william.eshop.model.ItemComment;
+import william.eshop.rest.CommonRestResponse;
+import william.eshop.service.ItemCommentService;
+import william.eshop.vo.ItemCommentVO;
+
+/**
+ * @Author zhangshenao
+ * @Date 2019-12-03
+ * @Description 商品评价API
+ */
+@Api(value = "商品评价相关接口")
+@RestController
+@RequestMapping("/itemComment")
+public class ItemCommentController {
+    @Autowired
+    private ItemCommentService commentService;
+
+    @GetMapping("/listByItem/{itemId}")
+    @ApiOperation(value = "查询指定商品的评价列表", httpMethod = "GET")
+    public CommonRestResponse<List<ItemCommentVO>> listByItem(@PathVariable("itemId") String itemId) {
+        List<ItemCommentVO> comments = Optional.ofNullable(commentService.listByItem(itemId))
+                .orElse(Collections.emptyList())
+                .stream()
+                .map(ItemComment::toVO)
+                .collect(Collectors.toList());
+        return CommonRestResponse.ok(comments);
+    }
+}
